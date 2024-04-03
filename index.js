a6222590a4d65f7a1835c5c42d9456d79175651a
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-03 23:26:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function swapRoles() {
+    var a = 10, b = 20;
+    console.log("before swap: a =", a, "before swap: b =", b);
+    var temp = a;
+    a = b;
+    b = temp;
+    console.log("After swap: a =", a, "After swap: b =", b);
+}
+swapRoles();
---
+++ index.ts
@@ -0,0 +1,10 @@
+function swapRoles(){
+    let a = 10, b =20;
+    console.log("before swap: a =",a,"before swap: b =" ,b);
+    let temp = a;
+    a = b;
+    b = temp;
+    console.log("After swap: a =",a,"After swap: b =" ,b);
+}
+
+swapRoles();
